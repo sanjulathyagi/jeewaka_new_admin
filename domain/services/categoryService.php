dae81f8f118dca378092e54acb9212a36bbd91b9
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2022-08-22 22:00:22 -0700

    a

--- domain/services/categoryService.php
+++ domain/services/categoryService.php
@@ -1,26 +1,28 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use domain\facades\categoryFacade;
 use App\Models\Category;
+
 use Illuminate\Http\Request;
 
 class CategoryController extends ParentController
 {
     public function index()
     {
-        $response['categories'] =Category::all();
+        $response['categories'] =categoryFacade::all();
         return view('pages.categories.index')->with($response);
     }
 
     public function new()
     {
-        return view('pages.categories.new');
+        $response['categories'] = categoryFacade::all();
+        return view('pages.categories.new')->with($response);
     }
 
     public function store(Request $request)
     {
-        Category::create($request->all());
+        categoryFacade::store($request->all());
         return redirect()->route('categories.all');
 
     }
