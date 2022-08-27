92396a6022e3bffce5e866fb3bfb4790685144ea
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2022-08-26 23:55:08 -0700

    A

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Product;
+use domain\facades\productFacade;
+
 use Illuminate\Http\Request;
 
 class ProductController extends ParentController
 {
     public function index()
     {
-        $response['products'] =Product::all();
+        $response['products'] =productFacade::all();
         return view('pages.products.index')->with($response);
     }
 }
--- domain/services/productService.php
+++ domain/services/productService.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\product;
 use Illuminate\Http\Request;
 
 class ProductController extends ParentController
 {
     public function index()
     {
-
+        return view('pages.products.index');
     }
 }
