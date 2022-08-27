b49585ff28b7e412385e033e7ce09f230d778d11
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2022-08-27 00:20:21 -0700

    a

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,10 +4,10 @@
     <section>
       <div class="page-header min-vh-100">
         <div class="container">
-          <div class="row">
-            <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
-              <div class="card card-plain">
-                <div class="card-header pb-0 text-start">
+          <div class="row ">
+            <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto ">
+              <div class="card card-plain ">
+                <div class="card-header pb-0 text-start ">
                   <h4 class="font-weight-bolder">Sign In</h4>
                   <p class="mb-0">Enter your email and password to sign in</p>
                 </div>
@@ -38,7 +38,8 @@
               </div>
             </div>
             <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
-              <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-ill.jpg');
+              <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
+               style="background-image: url('{{ asset('assets/img/c866864c-cdfe-49b6-92cf-352e9804fd34.jpg') }}');
           background-size: cover;">
                 <span class="mask bg-gradient-primary opacity-6"></span>
                 <h4 class="mt-5 text-white font-weight-bolder position-relative">"Attention is the new currency"</h4>
