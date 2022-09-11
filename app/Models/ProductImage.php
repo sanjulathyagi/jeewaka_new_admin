17c4caa83f5b685c1750af2652000bc1c43115b1
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2022-09-10 23:46:23 -0700

    a

---
+++ app/Models/ProductImage.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductImage extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'product_id',
+        'image_id',
+
+    ];
+
+    public function product()
+    {
+        return $this->hasOne(Product::class, 'id', 'product_id');
+    }
+
+    public function image()
+    {
+        return $this->hasOne(Image::class, 'id', 'image_id');
+    }
+
+
+
+}
