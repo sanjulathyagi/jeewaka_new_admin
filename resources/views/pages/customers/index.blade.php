61de6a204e01d22e45deda08b8c0f42b9ac5b138
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2022-09-16 00:44:05 -0700

    a

--- resources/views/pages/customers/index.blade.php
+++ resources/views/pages/customers/index.blade.php
@@ -2,18 +2,18 @@
 @section('header')
     <div class="row">
         <div class="col-lg-8">
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                    <li class="breadcrumb-item text-sm">
-                        <a class="opacity-5 text-white" href="{{ route('dashboard') }}">
-                            <i class="fa-solid fa-house"></i>
-                        </a>
-                    </li>
-                    <li class="breadcrumb-item text-sm text-white active" aria-current="page">customers</li>
-                </ol>
-                <h6 class="font-weight-bolder text-white mb-0"> All customers</h6>
-            </nav>
         </div>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+                <li class="breadcrumb-item text-sm">
+                    <a class="opacity-5 text-white" href="{{ route('dashboard') }}">
+                        <i class="fa-solid fa-house"></i>
+                    </a>
+                </li>
+                <li class="breadcrumb-item text-sm text-white active" aria-current="page">customers</li>
+            </ol>
+            <h6 class="font-weight-bolder text-white mb-0"> All customers</h6>
+        </nav>
         <div class="col-lg-4 text-right d-flex flex-column justify-content-center">
             <a href="{{ route('customers.new') }}"
                 class="btn btn-outline-white mb-0 ms-lg-auto me-lg-0 me-auto mt-lg-0 mt-2">New</a>
@@ -92,7 +92,7 @@
 @push('scripts')
     <script>
         $(document).ready(function() {
-            
+
             $('#customers_table').DataTable({
                 "language": {
                     "emptyTable": "No data available in the table",
