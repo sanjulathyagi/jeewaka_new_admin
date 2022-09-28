da80834cb1d4d4146f632566672dca9efbb0bfe9
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2022-09-27 23:19:05 -0700

    a

--- resources/views/libraries/scripts.blade.php
+++ resources/views/libraries/scripts.blade.php
@@ -132,6 +132,8 @@
 
      <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
+     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+
 
 <script>
     $(document).ready(function() {
