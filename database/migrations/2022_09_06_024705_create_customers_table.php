958e547e8129872356e7025b2faa272421ce8792
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2022-10-27 23:15:17 -0700

    a

--- database/migrations/2022_09_06_024705_create_customers_table.php
+++ database/migrations/2022_09_06_024705_create_customers_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
