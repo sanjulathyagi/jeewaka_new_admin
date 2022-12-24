6ce971eaf83e70d71a5f33bdea5c6c5335a68fb6
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2022-12-24 00:19:48 -0800

    a

---
+++ database/seeders/ExpenseTypeSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ExpenseType;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ExpenseTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = [
+            [
+                'name' => 'Food',
+            ],
+            [
+                'name' => 'Transportation',
+            ],
+            [
+                'name' => 'Entertainment',
+            ],
+            [
+                'name' => 'Shopping',
+            ],
+            [
+                'name' => 'Others',
+            ],
+        ];
+        foreach ($types as $type) {
+            ExpenseType::create($type);
+        }
+    }
+}
