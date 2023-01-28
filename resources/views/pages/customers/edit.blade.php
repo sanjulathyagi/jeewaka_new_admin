f692546e83b34cf6b8db3c45f7ae936a7e702c54
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2023-01-27 23:53:42 -0800

    a

--- resources/views/pages/customers/edit.blade.php
+++ resources/views/pages/customers/edit.blade.php
@@ -2,7 +2,7 @@
 @section('header')
     <div class="row">
         <div class="col-lg-8">
-            <nav aria-label="breadcrumb">
+            {{-- <nav aria-label="breadcrumb">
                 <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
                     <li class="breadcrumb-item text-sm">
                         <a class="opacity-5 text-white" href="{{ route('dashboard') }}">
@@ -17,7 +17,7 @@
                     <li class="breadcrumb-item text-sm text-white active" aria-current="page">Edit</li>
                 </ol>
                 <h6 class="font-weight-bolder text-white mb-0">Edit products</h6>
-            </nav>
+            </nav> --}}
         </div>
     </div>
 @endsection
