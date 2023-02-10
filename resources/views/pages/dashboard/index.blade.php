affcd190d0f1fcedb90f26952c082b6eeef190f0
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2023-02-09 23:12:22 -0800

    a

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -123,10 +123,10 @@
             <div class="card z-index-2 h-100">
                 <div class="card-header pb-0 pt-3 bg-transparent">
                     <h6 class="text-capitalize">Appointment calendar</h6>
-                    <p class="text-sm mb-0">
-                        <i class="fa fa-arrow-up text-success"></i>
-                        <span class="font-weight-bold">Total orders</span>
-                    </p>
+
+                </div>
+                <div class="card-body p-3">
+                    <div class="fullcalendar" data-color="primary" data-height="450"></div>
                 </div>
             </div>
         </div>
