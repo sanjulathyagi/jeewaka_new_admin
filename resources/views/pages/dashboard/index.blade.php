9d7c090e623dd0e912a3ec52769781e253583fc7
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2023-02-23 23:40:15 -0800

    a

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -118,7 +118,7 @@
             </div>
         </div>
     </div>
-    {{-- <div class="row mt-4">
+    <div class="row mt-4">
         <div class="col-lg-12 mb-lg-0 mb-4">
             <div class="card z-index-2 h-100">
                 <div class="card-header pb-0 pt-3 bg-transparent">
@@ -130,7 +130,7 @@
                 </div>
             </div>
         </div>
-    </div> --}}
+    </div>
 @endsection
 @push('js')
 <script>
